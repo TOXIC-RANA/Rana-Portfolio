c4f247e7d8f92f2d834494689ced9bcc05501931
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-07-29 08:13:59 +0600

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
+    duration: "2026 - Present",
     institution: "National University of Bangladesh",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2014 - 2026",
+    institution: "Barrister Jamir Uddin Sircur Collegiate Institute",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2018 - 2024",
+    institution: "Shalbahan High School",
   }
-]
+]  
