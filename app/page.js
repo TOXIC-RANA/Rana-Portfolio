89de878afe6d00b854dfbd64a6445f23a17ab5a4
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-07-29 17:33:40 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
+import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
@@ -25,15 +26,15 @@ export default async function Home() {
   const blogs = await getData();
 
   return (
-    <>
+    <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
       <Experience />
       <Skills />
       <Projects />
       <Education />
-      {/* <Blog blogs={blogs} /> */}
+      <Blog blogs={blogs} />
       <ContactSection />
-    </>
+    </div>
   )
 };
