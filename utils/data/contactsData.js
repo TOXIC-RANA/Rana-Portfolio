d58b81568bc910f4a6b3f24033b94a3226cadc24
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-07-29 08:08:53 +0600

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    email: 'dev.r4na@gmail.com',
+    phone: '+8801752865115',
+    address: 'Tetulia, Panchagarh, Rangpur, Bangladesh - 1212 ',
+    github: 'https://github.com/TOXIC-RANA',
+    facebook: 'https://www.facebook.com/XAICO.RANA',
     linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
     twitter: 'https://twitter.com/said7388',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    devUsername: "TOXIC-RANA"
 }
